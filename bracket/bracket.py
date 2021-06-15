15990b706e3d1ff7591ec210a34b7d589e01e5c6
Jonathan Belden <jonathan.belden@bluehalo.com>
2021-06-15 12:52:17 -0600

    added some abstraction to reduce function complexity

--- bracket/bracket.py
+++ bracket/bracket.py
@@ -8,6 +8,7 @@
 #              formatting irregularities)
 
 
+import bracket
 import os
 from icecream import ic
 ic.configureOutput(includeContext=True)
@@ -17,52 +18,38 @@ def is_valid(input_file):
     return os.path.isfile(input_file)
 
 
-def count_brackets(input_file, input_bracket, save_profile=False) -> int:
-    bracket_types = {"curly": ["{", "}"],
-                     "round": ["(", ")"],
-                     "square": ["[", "]"]}
-
+def analyze(input_file, input_bracket, save_profile=False) -> int:
     with open(input_file, "r") as file:
         content = file.readlines()
 
-    open_bracket = bracket_types[input_bracket][0]
-    close_bracket = bracket_types[input_bracket][1]
-    bracket_profile = {open_bracket: 0, close_bracket: 0, "lines": ""}
+    open_bracket = f"{input_bracket}_left"
+    close_bracket = f"{input_bracket}_right"
     if input_bracket == "curly":
         ic(input_bracket)
-        for i, line in enumerate(content):
-            if line.strip().startswith("#"):
-                continue
-            elif open_bracket in line:
-                bracket_profile["lines"] += f"{i+1} |  {line.replace(' ', '.')}"
-                bracket_profile[open_bracket] += 1
-            elif close_bracket in line:
-                bracket_profile["lines"] += f"{i+1} |  {line.replace(' ', '.')}"
-                bracket_profile[close_bracket] += 1
+        analysis_profile = count_curly_brackets(content)
+        bracket_count = analysis_profile[open_bracket] + analysis_profile[close_bracket]
+        description = "bracket"
+    
+    elif input_bracket == "single" or input_bracket == "double":
+        analysis_profile = count_quotes(content, input_bracket)
+        bracket_count = analysis_profile[f"{input_bracket}_quotes"]
+        description = "quotes"
+    
     else:
-        ic(input_bracket)
-        for i, line in enumerate(content):
-            if line.strip().startswith("#"):
-                continue
-            elif open_bracket in line or close_bracket in line:
-                open_count = line.count(open_bracket)
-                close_count = line.count(close_bracket)
-                bracket_profile[open_bracket] += open_count
-                bracket_profile[close_bracket] += close_count
+        analysis_profile = count_in_line_brackets(content, input_bracket)
+        bracket_count = analysis_profile[open_bracket] + analysis_profile[close_bracket]
+        description = "bracket"
 
-                if open_count != close_count:
-                    bracket_profile["lines"] += f"{i+1} [{open_count}:{close_count}]* |  {line}"
-                else:
-                    bracket_profile["lines"] += f"{i+1} [{open_count}:{close_count}] |  {line}"
-
-    bracket_count = bracket_profile[open_bracket] + bracket_profile[close_bracket]
     output = ""
     if not bracket_count % 2 == 0 or save_profile:
-        output += f"Total Bracket Count: {bracket_count}\n"
-        output += f"\t'{open_bracket}': {bracket_profile[open_bracket]}\n"
-        output += f"\t'{close_bracket}': {bracket_profile[close_bracket]}\n\n"
+        output += f"Total {input_bracket} quote count: {bracket_count}\n" if input_bracket == "single" or input_bracket == "double" else f"Total {input_bracket} bracket count: {bracket_count}\n"
+        try:
+            output += f"\t'{open_bracket}': {analysis_profile[open_bracket]}\n"
+            output += f"\t'{close_bracket}': {analysis_profile[close_bracket]}\n"
+        except KeyError:
+            pass
         output += f"=====================================================\n\n"
-        output += bracket_profile["lines"]
+        output += analysis_profile["lines"]
         print(output)
 
     if save_profile:
@@ -71,8 +58,62 @@ def count_brackets(input_file, input_bracket, save_profile=False) -> int:
         if not os.path.isdir(save_profile):
             os.mkdir(save_profile)
         
-        file_name = os.path.join(save_profile, f"{os.path.basename(input_file)}_{input_bracket}_bracket_profile.txt")
+        file_name = os.path.join(save_profile, f"{input_bracket}-{description}-profile_{os.path.basename(input_file)}")
         with open(file_name, "w") as file:
             file.write(output)
 
     return bracket_count
+
+
+def count_curly_brackets(content) -> dict:
+    bracket_profile = {"curly_left": 0, "curly_right": 0, "lines": ""}
+    for i, line in enumerate(content):
+            if line.strip().startswith("#") or line.strip().startswith("//"):
+                continue
+            elif "{" in line:
+                bracket_profile["lines"] += f"{i+1} |  {line.replace(' ', '.')}"
+                bracket_profile["curly_left"] += 1
+            elif "}" in line:
+                bracket_profile["lines"] += f"{i+1} |  {line.replace(' ', '.')}"
+                bracket_profile["curly_right"] += 1
+    return bracket_profile
+
+
+def count_in_line_brackets(content, bracket_type) -> dict:
+    in_line_brackets = {"angle": ["<", ">"],
+                        "round": ["(", ")"],
+                        "square": ["[", "]"]}
+    left_bracket = in_line_brackets[bracket_type][0]
+    right_bracket = in_line_brackets[bracket_type][1]
+    bracket_profile = {f"{bracket_type}_left": 0, f"{bracket_type}_right": 0, "lines": ""}
+    for i, line in enumerate(content):
+        if line.strip().startswith("#") or line.strip().startswith("//"):
+            continue
+        elif left_bracket in line or right_bracket in line:
+            left_count = line.count(left_bracket)
+            right_count = line.count(right_bracket)
+            bracket_profile[f"{bracket_type}_left"] += left_count
+            bracket_profile[f"{bracket_type}_right"] += right_count
+
+            if left_count != right_count:
+                bracket_profile["lines"] += f"{i+1} [{left_count}:{right_count}]* |   {line}"
+            else:
+                bracket_profile["lines"] += f"{i+1} [{left_count}:{right_count}]  |   {line}"
+    return bracket_profile
+
+
+def count_quotes(content, input_quote) -> dict:
+    quote_types = {"double": "\"", "single": "'"}
+    quotes_profile = {f"{input_quote}_quotes": 0, "lines": ""}
+    for i, line in enumerate(content):
+        if line.strip().startswith("#") or line.strip().startswith("//"):
+            continue
+        elif quote_types[input_quote] in line:
+            qoute_count = line.count(quote_types[input_quote])
+            quotes_profile[f"{input_quote}_quotes"] += qoute_count
+
+            if qoute_count % 2 != 0:
+                quotes_profile["lines"] += f"{i+1} [{qoute_count}]* |   {line}"
+            else:
+                quotes_profile["lines"] += f"{i+1} [{qoute_count}]  |   {line}"
+    return quotes_profile
